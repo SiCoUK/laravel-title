91e08d9e495afdaf53f5c50876d87b4e415d07d1
Simon Corless <simon@sico.co.uk>
2017-04-13 11:47:18 +0100

    Set and display titles, allow setting of a specific page title.

--- src/Title.php
+++ src/Title.php
@@ -3,8 +3,89 @@ namespace SiCoUK\LaravelTitle;
 
 class Title
 {
-    public function add($title)
+    protected $title = [];
+    protected $pageTitle;
+    
+    protected $seperator = ' - ';
+    
+    /**
+     * Add a title to the title collection
+     *
+     * @param $item
+     * @return mixed
+     */
+    public function add($item, $setAsPageTitle = false)
     {
-        return 'Added Title';
+        if (is_array($item)) {
+            return array_map([$this, 'add'], $item);
+        }
+        if (!$item | strlen(trim($item)) === 0) {
+            return false;
+        }
+        $this->title[] = trim(strip_tags($item));
+        
+        if ($setAsPageTitle) {
+            $this->setPageTitle($item);
+        }
+    }
+    
+    /**
+     * Return the last entry in the collection as the current page title
+     * 
+     * @return string
+     */
+    public function getPageTitle()
+    {
+        if (!empty($this->pageTitle)) {
+            return $this->pageTitle;
+        }
+        
+        if (empty($this->title)) {
+            // Return the default app name
+            return \config::get('app.name');
+        }
+        
+        return array_pop($this->title);
+    }
+    
+    /**
+     * 
+     * 
+     * @param string $title
+     */
+    public function setPageTitle($title)
+    {
+        $this->pageTitle = $title;
+    }
+    
+    /**
+     * Set the title seperator
+     * 
+     * @param string $seperator
+     */
+    public function setSeperator($seperator)
+    {
+        $this->seperator = $seperator;
+    }
+    
+    /**
+     * Return the full title as a string
+     * 
+     * @return string
+     */
+    public function toString()
+    {
+        if (empty($this->title)) {
+            // Return the default app name
+            return \config::get('app.name');
+        }
+        
+        // Add the app title to the begining
+        $title = array_merge([\config::get('app.name')], $this->title);
+        
+        // Reverse the array for the page title
+        $title = array_reverse($title);
+        
+        return implode($this->seperator, $title);
     }
 }
